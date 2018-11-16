b90e82f76541663f5faa80db1d6c26f78a3aabc8
netshiaMR <netshiarendani@gmail.com>
2018-11-16 18:39:42 +0200

    Jobhaven Database JobhavenConnector Class Renaming and path update.

---
+++ src/main/java/com/jaxws/jobhaven/api/model/connections/JobhavenConnector.java
@@ -0,0 +1,56 @@
+package com.jaxws.jobhaven.api.model.connections;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+/**
+ * @author NetshiaR2
+ */
+@Stateless
+public class JobhavenConnector {
+
+    DataSource jobhavenDBSource;
+
+    @PostConstruct
+    public void init() {
+        try {
+            InitialContext ctx = new InitialContext();
+            jobhavenDBSource = (DataSource) ctx.lookup("jdbc/jobhavendb");
+        } catch (Exception ex) {
+           System.out.println("Unable to get DataSource connection");
+        }
+    }
+
+    public void releaseResources(PreparedStatement pstmt, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        try (Connection conn = pstmt.getConnection()) {
+            pstmt.close();
+        }
+    }
+
+    public void releaseResources(CallableStatement cstmt, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        try (Connection conn = cstmt.getConnection()) {
+            cstmt.close();
+        }
+    }
+
+    public Connection getJobhavenDbConnection() throws SQLException {
+        if (jobhavenDBSource == null) {
+            init();
+        }
+        return jobhavenDBSource.getConnection();
+    }
+
+}
